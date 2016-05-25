b91cd79e8bcd0c9d59b47ce2677182236a571309
Josh <you@example.coexit>
2016-05-25 05:16:31 -0500

    js working

---
+++ scripts/main.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+	// Set Options
+	var speed = 500; 				// Transition fade speed
+	var autoswitch = true;  		// Auto slider options
+	var autoswitch_speed = 4000 	// Auto slider speed
+
+	// Add initial active class
+	$('.slide').first().addClass('active');
+
+	// Hide all slides
+	$('.slide').hide();
+
+	// Show first slide
+	$('.active').show();
+
+
+	// Next Handler
+	$('#next').on('click', nextSlide);
+
+	// Previous Handler
+		$('#prev').on('click', prevSlide);
+
+	// Auto slider Handler
+	if(autoswitch == true) {
+		setInterval(nextSlide,autoswitch_speed);
+	}
+
+	// Switch to next slide
+	function nextSlide(){
+		$('.active').removeClass('active').addClass('oldActive');
+		if($('.oldActive').is(':last-child')){
+			$('.slide').first().addClass('active');
+		} else  {
+			$('.oldActive').next().addClass('active');
+		}
+		$('.oldActive').removeClass('oldActive');
+		$('.slide').fadeOut(speed);
+		$('.active').fadeIn(speed);
+	}
+
+	// Switch to previous slide
+	function prevSlide() {
+		$('.active').removeClass('active').addClass('oldActive');
+		if($('.oldActive').is(':first-child')){
+			$('.slide').last().addClass('active');
+		} else  {
+			$('.oldActive').prev().addClass('active');
+		}
+		$('.oldActive').removeClass('oldActive');
+		$('.slide').fadeOut(speed);
+		$('.active').fadeIn(speed);
+	}
+
+});
